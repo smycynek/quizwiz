1523dcbb2c626a759a9ff237d371a63e522bcfb1
Steven Mycynek <smycynek@buildingengines.com>
2020-11-23 16:03:30 -0500

    social media links

--- src/components/TakeQuiz.js
+++ src/components/TakeQuiz.js
@@ -10,6 +10,11 @@ import { Helmet } from 'react-helmet';
 
 import quizFreakClient from '../api/quizFreakClient';
 
+import twitter from '../images/twitter.svg';
+import facebook from '../images/facebook.svg';
+import pinterest from '../images/pinterest.svg';
+import linkedin from '../images/linkedin.svg';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../index.css';
 
@@ -19,6 +24,9 @@ const TakeQuiz = ({ match }) => {
   const [quizData, setQuizData] = useState(null);
   const [waiting, setWaiting] = useState(false);
   const [error, setError] = useState(false);
+  const [quizTwitter, setQuizTwitter] = useState('');
+  const [quizLinkedIn, setQuizLinkedIn] = useState('');
+  const [quizFacebook, setQuizFacebook] = useState('');
 
   function fetchQuiz(quizId) {
     setWaiting(true);
@@ -28,6 +36,13 @@ const TakeQuiz = ({ match }) => {
       const { data } = res;
       setQuizData(data);
       setWaiting(false);
+      const quizUrl = `https://stevenvictor.net/quizwiz/%23/quizwiz/take/${quizId}`;
+      const twitterLink = `http://twitter.com/share?text=${data.name}&url=${quizUrl}&hashtags=quizwiz`;
+      const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=${quizUrl}`;
+      const linkedinLink = `https://www.linkedin.com/sharing/share-offsite/?mini=true&url=${quizUrl}&title=${data.name}&source=stevenvictor.net`;
+      setQuizFacebook(facebookLink);
+      setQuizTwitter(twitterLink);
+      setQuizLinkedIn(linkedinLink);
     };
 
     const failure = (err) => {
@@ -64,7 +79,19 @@ const TakeQuiz = ({ match }) => {
           questions={quizData.questions}
         />
         )}
+        <div>
 
+          <a href={quizTwitter} target="_blank" rel="noopener noreferrer">
+            <img className="socialMedia" alt="twitter" src={twitter} />
+          </a>
+          <a href={quizLinkedIn} target="_blank" rel="noopener noreferrer">
+            <img className="socialMedia" alt="linkedin" src={linkedin} />
+          </a>
+          <a href={quizFacebook} target="_blank" rel="noopener noreferrer">
+            <img className="socialMedia" alt="facebook" src={facebook} />
+          </a>
+          <p>Share this quiz!</p>
+        </div>
         <div>
           <Link to="/">Home...</Link>
         </div>
